ad2b77ed801cec09c9e88f8d2b5f9d43af64d9cb
William Martin <williammartincruz@gmail.com>
2014-05-12 13:50:36 -0500

    made changes to game

---
+++ highlow.php
@@ -0,0 +1,34 @@
+<?php
+// create a number
+// while the user hasn't guessed correctly
+	// prompt the user "to guess number"
+	// get number from user
+	// check the number and the guess
+	// if the guess is higher than the number then echo "lower"
+	// if the guess is lower than the number then echo "higher"
+	// add to the guess count
+// end the loop
+	// if guess and the number are the same say "winner" and end game
+fwrite(STDOUT, 'Guess a number between 1 and 10.' . "\n");
+
+$number = rand(0, 10);
+
+// Get the input from user
+
+$guess = 0;
+$nug = 0;
+
+while ($guess != $number) {
+	$guess = (int) fgets(STDIN);
+	$nug++;
+	if ($guess < $number) {
+	// If the number is too low print
+		fwrite(STDOUT, "higher\n");	
+	} elseif ($guess > $number) {
+	// If the number too high print	
+		fwrite(STDOUT, "lower \n");
+	} else {
+	// fwrite(STDOUT, "You guessed correctly in {$nug}, tries.  \n");
+	fwrite(STDOUT, "GOOD JOB! It took you $nug tries to guess this number.\n");
+}} 
+// Output Diplay number of guesses.
