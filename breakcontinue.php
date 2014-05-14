bdfcbba566f214eb59c5e57ccf9cea40aa14c410
William Martin <williammartincruz@gmail.com>
2014-05-14 11:11:32 -0500

    continue exercise added

--- breakcontinue.php
+++ breakcontinue.php
@@ -1,8 +1,18 @@
 <?php
 
-for ($i = 2; $i <= 100; $i += 2) {
+// for ($i = 2; $i <= 100; $i += 2) {
+// 	echo $i . "\n";
+// 	if ($i % 2 == 0);
+// 		continue;
+// 	}
+// 	echo "Done";
+
+
+for ($i = 1; $i <= 100; $i++) {
 	echo $i . "\n";
-	if ($i % 2 == 0);
-		continue;
+	if ($i >= 10) {
+		break;
 	}
-	echo "Done";
+	
+}
+echo "Done\n";
