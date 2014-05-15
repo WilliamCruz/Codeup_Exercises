a02959444daa1c35d390ac8268703739aff3ad4a
William Martin <williammartincruz@gmail.com>
2014-05-15 10:10:15 -0500

    adding to repo

---
+++ arithmetic.php
@@ -0,0 +1,40 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b . PHP_EOL;
+}
+
+add(4,5);
+add(65,88);
+add(37,34);
+add(3.5,20);
+
+function subtract($a, $b) {
+	echo $a - $b . PHP_EOL;
+    // Add code here
+}
+
+subtract(4,5);
+subtract(65,88);
+subtract(37,34);
+subtract(3.5,20);
+
+function multiply($a, $b) {
+	echo $a * $b . PHP_EOL;
+    // Add code here
+}
+
+multiply(4,5);
+multiply(65,88);
+multiply(37,34);
+multiply(3.5,20);
+
+function divide($a, $b) {
+	echo $a / $b . PHP_EOL;
+    // Add code here
+}
+
+divide(4,5);
+divide(65,88);
+divide(37,34);
+divide(3.5,20);
