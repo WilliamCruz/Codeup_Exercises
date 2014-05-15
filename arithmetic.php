efa8092c8f86c4ac1ccf58cb71530bb7681b76d3
William Martin <williammartincruz@gmail.com>
2014-05-15 13:38:03 -0500

    add if statement

--- arithmetic.php
+++ arithmetic.php
@@ -1,52 +1,68 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b . PHP_EOL;
+    if (is_numeric($a) && is_numeric($b)) {
+		echo $a + $b . PHP_EOL;
+  } else {
+		echo 'ERROR: Both arguments should be numbers' . PHP_EOL;
+	}
 }
 
-add(4,5);
-add(65,88);
-add(37,34);
-add(3.5,20);
+add(5, 2);
+
 
 function subtract($a, $b) {
-	echo $a - $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a - $b . PHP_EOL;
+  } else {
+  		echo 'ERROR: Both arguments should be numbers' . PHP_EOL;
+    } 
     // Add code here
 }
 
-subtract(4,5);
-subtract(65,88);
-subtract(37,34);
-subtract(3.5,20);
+subtract(4,7);
+// subtract(65,88);
+// subtract(37,34);
+// subtract(3.5,20);
 
 function multiply($a, $b) {
-	echo $a * $b . PHP_EOL;
-    // Add code here
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a * $b . PHP_EOL;   
+  } else {
+  		echo 'ERROR: Both arguments should be numbers' . PHP_EOL;
+    }   // Add code here 
 }
-
 multiply(4,5);
-multiply(65,88);
-multiply(37,34);
-multiply(3.5,20);
+// multiply(65,88);
+// multiply(37,34);
+// multiply(3.5,20);
 
 function divide($a, $b) {
-	echo $a / $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a / $b . PHP_EOL;
+  } else {
+  		echo 'ERROR: Both arguments should be numbers' . PHP_EOL;
+    }
     // Add code here
 }
 
-divide(4,5);
-divide(65,88);
-divide(37,34);
-divide(3.5,20);
+divide(4,2);
+// divide(65,88);
+// divide(37,34);
+// divide(3.5,20);
 
 function modulus($a, $b) {
-	echo $a % $b . PHP_EOL;
-}
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a % $b . PHP_EOL;
+  } else {
+  		echo 'ERROR: Both arguments should be numbers' . PHP_EOL;
+  	}
+}	
 
-modulus(4,5);
-modulus(65,88);
-modulus(37,34);
-modulus(3.5,20);
+modulus(4,2);
+// modulus(65,88);
+// modulus(37,34);
+// modulus(3.5,20);
 
 
 
