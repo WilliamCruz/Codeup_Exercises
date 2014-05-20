1c521ce5e93636d03f3812b5deeb7536bd00c554
William Martin <williammartincruz@gmail.com>
2014-05-20 14:43:19 -0500

    added function

--- famous_fake_physicists.php
+++ famous_fake_physicists.php
@@ -4,23 +4,31 @@
 
 //Create the $famous_fake_physicists string that lists the physicists and contains the "and" at the end.
 // This should end with this line:
+//Turn your solution into a function named humanized_list(). You should be able to pass the $physicists_array
+//as the only argument, and your function will return the result.
+//Example code:
 
 
 // Converts array into list n1, n2, ..., and n3
-// function humanized_list($array) {
+function humanized_list($string) 
+{
+	$array = explode(', ', $string);
+	$last = array_pop($array);
+    $famous_fake_physicists = implode(', ', $array);
+	
+	$newString = "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}, and $last." . PHP_EOL;
+	return $newString;
+
   // Your solution goes here!
-// }
+}
 
-// List of famous peeps
 $physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+// List of famous peeps
+echo humanized_list($physicists_string) . PHP_EOL;
 
-$array_physicists = explode(', ', $physicists_string);
-
-$last = array_pop($array_physicists);
-$last = "and $last";
-array_push($array_physicists, $last);
-$famous_fake_physicists = implode(', ', $array_physicists);
-// Output sentence
-echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}." . PHP_EOL;
+// $array_physicists = explode(', ', $physicists_string);
+//print_r($physicists_array);
+// array_push($array_physicists, $last);
+// // Output sentence
 
 ?>
