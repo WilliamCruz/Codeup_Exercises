0cf219466893717f7d2cd62af6ccf426f0594f01
William Martin <williammartincruz@gmail.com>
2014-05-31 14:17:26 -0500

    named file according to exercise/ added heading comments

---
+++ search_arrays.php
@@ -0,0 +1,43 @@
+<?php
+//WIlliam Cruz
+//5/31/14
+//Array Functions - Searching in arrays
+//Complete and review exercise
+
+
+// first names
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+ function array_has_value($needle, $haystack)
+{
+	if (array_search($needle, $haystack) === false)
+	  {
+		return false;
+      } 
+      else 
+      {
+	    return true;
+	}
+
+}
+var_dump(array_has_value('Bob', $names));
+
+
+function array_common_count($array1, $array2)
+{
+	$count = 0;
+	foreach ($array1 as $key => $value)
+	{
+		// echo $value . PHP_EOL;
+		if (array_has_value($value, $array2))
+		{
+			$count++;
+		}
+	}
+	
+	return $count;
+}
+
+var_dump(array_common_count($names, $compare));
