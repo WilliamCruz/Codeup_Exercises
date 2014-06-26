e63411126f4ed1c2740a29784abffa41e5b6727e
William Martin <williammartincruz@gmail.com>
2014-06-26 18:40:07 -0500

    converted indentation to spaces

--- foreach.php
+++ foreach.php
@@ -6,6 +6,6 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 foreach ($things as $thing) 
 {
-	if (is_scalar($thing)) {
-}	echo "{$thing}" . PHP_EOL;
+    if (is_scalar($thing)) {
+}   echo "{$thing}" . PHP_EOL;
 } 
