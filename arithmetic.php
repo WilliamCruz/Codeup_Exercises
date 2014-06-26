4513f1d07da405c1719f0678da98d6e53271ae4c
William Martin <williammartincruz@gmail.com>
2014-06-26 17:18:03 -0500

    adjusted indention on last function using sublime's convert indentation to spaces

--- arithmetic.php
+++ arithmetic.php
@@ -1,30 +1,30 @@
 <?php
 
 function add($a, $b) {
-	error_msg($a, $b);
-	return $a + $b;	
+    error_msg($a, $b);
+    return $a + $b; 
 }
 function subtract($a, $b) {
-	error_msg($a, $b);
-	return $a - $b;
+    error_msg($a, $b);
+    return $a - $b;
 }
 function multiply($a, $b) {
-	error_msg($a, $b);
-	return $a * $b;    
+    error_msg($a, $b);
+    return $a * $b;    
 }
 function divide($a, $b) {
-	error_msg($a, $b);
-	return $a / $b;
+    error_msg($a, $b);
+    return $a / $b;
 }
 function modulus($a, $b) {
-	error_msg($a, $b);
-  	return $a % $b; 
-}	
+    error_msg($a, $b);
+    return $a % $b; 
+}   
 //checking for numeric input, echo error msg if not numeric otherwise, continue
 function error_msg($a, $b) {
-	if (!is_numeric($a) || !is_numeric($b)) {
-		echo "$a and $b should both be a number" . PHP_EOL;
-	}
+    if (!is_numeric($a) || !is_numeric($b)) {
+        echo "$a and $b should both be a number" . PHP_EOL;
+    }
 }
 echo add(5, 0) . PHP_EOL;
 echo subtract(4,5) . PHP_EOL;
