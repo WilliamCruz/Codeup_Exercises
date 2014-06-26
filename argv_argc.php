396e9fb94b52717d15644d58e9a62c164cf8b0c4
William Martin <williammartincruz@gmail.com>
2014-06-26 16:59:43 -0500

    removed git notes and adjusted spacing.This is just an example of argv and argc arguments

--- argv_argc.php
+++ argv_argc.php
@@ -1,16 +1,8 @@
-
-
-// mkdir []
-
-// git [comman] [option 1] o[ption 2] ...
-// e.g. git remote add origin [url]
-
 <?php
 
 echo "\$argc";
 // Dump arg count
 var_dump($argc);
-
 echo "$argc";
 // Dump arg vars
 var_dump($argv);
