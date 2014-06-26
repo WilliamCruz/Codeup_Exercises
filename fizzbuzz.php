7126606e2ab8ca9e5be6653fccf146c21c7f5afd
William Martin <williammartincruz@gmail.com>
2014-06-26 18:27:43 -0500

    removed comments and empty space at bottom

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,14 +1,6 @@
 <?php
 
-
-// Write a program that prints the numbers from 1 to 100. 
-// But for multiples of three print “Fizz” instead of the number 
-// and for the multiples of five print “Buzz”. For numbers which 
-// are multiples of both three and five print “FizzBuzz”
-
 for ($n = 1; $n <= 100; $n++) {
-	// echo "$n \n";
-
 	if ($n % 3 == 0 && $n % 5 == 0) {
 		echo "FizzBuzz\n";
 	} elseif ($n % 3 == 0) {
@@ -18,13 +10,4 @@ for ($n = 1; $n <= 100; $n++) {
 	} else {
 		echo "$n\n";
 	}
-// for ($n = 1; $n <= 100; $n++) {
-// 	echo "$n \n";
-
 }
-
-
-// for ($n = 1; $n <= 100; $n + 5) {
-// 	if ($n % 5);
-// 	echo ("Buzz");
-
