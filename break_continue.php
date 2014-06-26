4fac7afeeb0b47ab8c5c98ef7dda6cc7ad37f869
William Martin <williammartincruz@gmail.com>
2014-06-26 17:50:47 -0500

    uncommented continue for loop, tested, and adjusted spacing

--- break_continue.php
+++ break_continue.php
@@ -1,26 +1,20 @@
 <?php
-//William Cruz
-//5/29/14
-//break and continue lesson
-//Create a file named break_continue.php in your exercises directory. 
-//Commit and push to GitHub after each step.
-//Create a for loop that shows all even numbers between 1 and 100 using continue.
-//Create another for loop that counts from 1 to 100, but stops after 10 using break.
-
+//WIlliam Cruz
+//5/31/14
+//Control Structures - break and continue exercises
 
 for ($i = 2; $i <= 100; $i += 2) {
 	echo $i . "\n";
-	if ($i % 2 == 0);
+	if ($i % 2 == 0){
 		continue;
 	}
 	echo "Done";
-
+}
 
 for ($i = 1; $i <= 100; $i++) {
 	echo $i . "\n";
 	if ($i >= 10) {
 		break;
 	}
-	
 }
 echo "Done\n";
