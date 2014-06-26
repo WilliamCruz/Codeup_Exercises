a4c2ccc604eec75b19aed1e5d65fb44696f003fb
William Martin <williammartincruz@gmail.com>
2014-06-26 17:25:13 -0500

    correction to variable on line 15, edited comments, and eliminated spacing at bottom

---
+++ array_names.php
@@ -0,0 +1,40 @@
+<?php
+
+$names = ['Marc Andreessen', 'Tim Berners-Lee', 'Len Bosack', 'Steve Case', 'Vint Cerf', 'Len Kleinrock', 'J.C.R. Licklider', 'Bob Metcalfe', 'Ray Tomlinson'];
+
+$query = 'Tim Berners-Lee';
+
+$result = array_search($query, $names);
+
+echo "Result is key {$result} in array \n "
+
+if ($result) {
+    echo $names[$result] . PHP_EOL;
+}
+
+$snake = [1,2,3,4,5];
+
+print_r($snake)
+
+// Remove first item
+array_shift($snake);
+// Append item
+array_push($snake, 6);
+
+function fix_name($name) {
+    return ucwords(trim($name));
+}
+echo fix_name("\t\tjason strawn\n") . PHP_EOL;)
+
+$my_name = fix_name("\t\tjason strawn\n") . PHP_EOL;
+
+echo $my_name;
+
+//implode and explode arrays
+$physicists_string = 'Gordon Freeman,Samanthat Carter, Sheldon Cooper, Quinn MAllory, Bruce Banner, Tony Stark';
+ ////////Delimeter is the comma in this case and this is where the explode function has been called in this example.
+$physicists_array = explode(',',$physicists_string);
+
+print_r($physicists_array);
+
+?>
