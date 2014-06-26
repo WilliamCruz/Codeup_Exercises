6c2a60f37bc701074954801e7c3bc91d826612a5
William Martin <williammartincruz@gmail.com>
2014-06-26 18:35:48 -0500

    removed comments and adjusted spacing

--- for_loop.php
+++ for_loop.php
@@ -1,22 +1,14 @@
 <?php
-echo "Enter a starting number.\n";
-
+fwrite(STDOUT, "Enter a starting number.\n");
 $low = trim(fgets(STDIN));
 
-echo "Now enter the ending number.\n";
-
+fwrite(STDOUT, "Now enter the ending number.\n");
 $high = trim(fgets(STDIN));
 
-echo "Enter increment \n";
-
+fwrite(STDOUT, "Enter increment.\n");
 $inc = trim(fgets(STDIN));
-
 
 for ($n = $low; $n <= $high; $n = $n + $inc) {
 	echo "\n$n\n";
 }
-
-// for ($en = 100; $en <= 100; $en -= 1); {
-// 	echo "$en";
-
-// }	
+	
