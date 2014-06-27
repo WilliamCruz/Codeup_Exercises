10fd1725dad5e075333f32b361e9a323790ce76e
William Martin <williammartincruz@gmail.com>
2014-06-26 20:24:36 -0500

    removed comments and adjusted indentation

--- highlow.php
+++ highlow.php
@@ -1,45 +1,28 @@
 <?php
-// create a number
-// while the user hasn't guessed correctly
-	// prompt the user "to guess number"
-	// get number from user
-	// does the range make sense
-	// check range of numbers and the guess
-	// if the guess is higher than the number then echo "lower"
-	// if the guess is lower than the number then echo "higher"
-	// add to the guess count
-// end the loop
-	// if guess and the number are the same say "winner" and end game
+
 if (!is_numeric($argv[1]) || !is_numeric($argv[2])) {
-	echo "I have no idea what you're talking about buddy. \n";
-	exit(1);
+    echo "I have no idea what you're talking about buddy. \n";
+    exit(1);
 }
 if ($argc == 3) {
 } else if($argv[1] != 0 || $argv[2] != 0) {
-	$argv++;
-
+    $argv++;
 }
 
 fwrite(STDOUT, "Guess a number between $argv[1] and $argv[2]." . "\n");
 
 $number = mt_rand($argv[1], $argv[2]);
-// Get the input from user
-// $argv[1] = min(1);
-// $argv[2] = max(100];
 $guess = 0;
 $nug = 0;
 
 while ($guess != $number) {
-	$guess = (int) fgets(STDIN);
-	$nug++;
-	if ($guess < $number) {
-	// If the number is too low print
-		fwrite(STDOUT, "higher\n");	
-	} elseif ($guess > $number) {
-	// If the number too high print	
-		fwrite(STDOUT, "lower \n");
-	} else {
-	// fwrite(STDOUT, "You guessed correctly in {$nug}, tries.  \n");
-	fwrite(STDOUT, "GOOD JOB! It took you $nug tries to guess this number.\n");
-}} 
-// Output Diplay number of guesses.
+    $guess = (int) fgets(STDIN);
+    $nug++;
+    if ($guess < $number) {
+        fwrite(STDOUT, "higher\n"); 
+    } elseif ($guess > $number) {
+        fwrite(STDOUT, "lower \n");
+    } else {
+    fwrite(STDOUT, "GOOD JOB! It took you $nug tries to guess this number.\n");
+}
+}   
