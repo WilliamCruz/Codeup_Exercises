ef221078179c9c5f22280d031e8a28318b8c9b42
William Martin <williammartincruz@gmail.com>
2014-06-28 23:34:19 -0500

    adjusted spacing, converted indentation to spaces

--- internal_functions.php
+++ internal_functions.php
@@ -4,7 +4,7 @@ $nothing = NULL;
 $something = '';
 $array = array(1,2,3);
 
-// Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+// Create a function that checks if a variable isset or empty, and display "$variable_name is SET|EMPTY"
 function  check($var) {
 	if (isset($var)) {
 		echo 'Variable is SET'. PHP_EOL;
@@ -14,19 +14,15 @@ function  check($var) {
    } 
 
 } 
-
+// unset($nothing);
 check($nothing);
 check($something);
 check($array);
-
-// TEST: If var $nothing is set, display '$nothing is SET'
-
-// TEST: If var $nothing is empty, display '$nothing is EMPTY'
-
-// TEST: If var $something is set, display '$something is SET'
+// // TEST: If var $nothing is set, display '$nothing is SET'
+// // TEST: If var $nothing is empty, display '$nothing is EMPTY'
+// // TEST: If var $something is set, display '$something is SET'
 
 // Serialize the array $array, and output the results
-
 $ser = serialize($array) ;
 	echo $ser;
 
@@ -34,3 +30,4 @@ $ser = serialize($array) ;
 
 $uns = unserialize($ser);
  	var_dump ($uns); 
+ 	
