eff0a37065f93cc2992d2ade5bc33e194f5b0cc0
William Martin <williammartincruz@gmail.com>
2014-06-29 16:36:07 -0500

    converted indentation to spaces, refactored line 21 to say even and line 23 to say odd, added else statement, moved line 23 into the if else argument

--- spi.php
+++ spi.php
@@ -2,13 +2,13 @@
 
 // // Write the output
 // // Notice the space after the ?
-// fwrite(STDOUT, 'What is your first name? ');
+fwrite(STDOUT, 'What is your first name? ');
 
 // // Get the input from user
-// $first_name = fgets(STDIN);
+$first_name = fgets(STDIN);
 
 // // Output the user's name
-// fwrite(STDOUT, "Hello $first_name\n");
+fwrite(STDOUT, "Hello $first_name\n");
 
 // Write the output
 // Notice the space after the ?
@@ -17,8 +17,10 @@ fwrite(STDOUT, 'Guess a number? ');
 // Get the input from user
 $number = (int)fgets(STDIN);
 
-If ($number % 2 == 0) {
-	fwrite(STDOUT, "{$number} is odd\n");
+if ($number % 2 == 0) {
+	fwrite(STDOUT, "{$number} is even.\n");
+} else {
+	fwrite(STDOUT, "{$number} is odd.\n");
 }
-// Output the user's name
-fwrite(STDOUT, "{$number} is even \n");
+
+?>
