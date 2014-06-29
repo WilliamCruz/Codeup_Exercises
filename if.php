a0c1c41ed9076cda8fbf10742c58066355295238
William Martin <williammartincruz@gmail.com>
2014-06-28 23:21:14 -0500

    adjusted spacing but still need to complete ternary argument

--- if.php
+++ if.php
@@ -1,17 +1,16 @@
 <?php
 
-// $a = 5;
-// $b = 10;
-// $c = '10';
+$a = 5;
+$b = 10;
+$c = '10';
 
-// if ($c == $b) {
-//     echo "$c is equal to $b \n";
-// }
+if ($c == $b) {
+    echo "$c is equal to $b \n";
+}
 
-// if ($c != $b) {
-//     echo "$c is not equal to $b \n";
-
-// }
+if ($c != $b) {
+    echo "$c is not equal to $b \n";
+}
 
 
 $a = 5;
@@ -19,29 +18,23 @@ $b = 10;
 $c = '10';
 
 if ($a == $b) {
-	echo "#a is equal to $b\n";
+	echo "$a is equal to $b\n";
 } else {
-	echo "$a is not equal to $b\n"
+	echo "$a is not equal to $b\n";
 }
 
-$my_page = 29;
-
-// [ condition ] ? [ value if true] : [ value if false]; 
-
-$can_see_die_hard = ($my_age >= 18) ? "yes!" : "no, try Disney";
-
-
-
-
+// $my_age = 29;
+// // [ condition ] ? [ value if true] : [ value if false]; 
+// $can_see_die_hard = [($my_age >= 18)] ? "yes!\n" : "no, try Disney\n";
 
 
 $a = 10;
 $b = '10';
 
 if ($a --- $b) {
-	echo "$a is identical to $b";
+	echo "$a is identical to $b\n";
 } elseif ($a == $b) {
-	echo "$a is equal to $b";
+	echo "$a is equal to $b\n";
 } elseif ($a < $b) {
-	echo "$a is less than $b";
+	echo "$a is less than $b\n";
 }
