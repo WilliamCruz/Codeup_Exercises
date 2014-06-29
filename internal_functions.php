dd531fc20d22f3bcbccb38c51530c27f302dcbf0
William Martin <williammartincruz@gmail.com>
2014-06-28 23:36:08 -0500

    removed comments and adjusted spacing

--- internal_functions.php
+++ internal_functions.php
@@ -6,28 +6,22 @@ $array = array(1,2,3);
 
 // Create a function that checks if a variable isset or empty, and display "$variable_name is SET|EMPTY"
 function  check($var) {
-	if (isset($var)) {
-		echo 'Variable is SET'. PHP_EOL;
-  } elseif (empty($var)) { 
-  		echo 'Variable is empty' . PHP_EOL;
-
-   } 
+    if (isset($var)) {
+        echo 'Variable is SET'. PHP_EOL;
+    } elseif (empty($var)) { 
+        echo 'Variable is empty' . PHP_EOL;
+    } 
 
 } 
-// unset($nothing);
 check($nothing);
 check($something);
 check($array);
-// // TEST: If var $nothing is set, display '$nothing is SET'
-// // TEST: If var $nothing is empty, display '$nothing is EMPTY'
-// // TEST: If var $something is set, display '$something is SET'
+
 
 // Serialize the array $array, and output the results
 $ser = serialize($array) ;
-	echo $ser;
+    echo $ser;
 
 // Unserialize the array $array, and output the results
-
 $uns = unserialize($ser);
- 	var_dump ($uns); 
- 	
+    var_dump ($uns); 
