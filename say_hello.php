083efc27053cff8680fb17df171dbd65f5f13d71
William Martin <williammartincruz@gmail.com>
2014-06-29 16:09:48 -0500

    removed spacing

--- say_hello.php
+++ say_hello.php
@@ -5,9 +5,7 @@ if ($argc < 2) {
     // echo "Usage: $progName [your name] \n";
     echo "Hey! I need your name to say hello to you! \n";
     exit(1);
-   
 }
 $name = $argv[1];
-
 echo "Hello " . $name . "\n";
 ?>
