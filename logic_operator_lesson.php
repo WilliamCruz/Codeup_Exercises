cbe561e3036f79fe18a1b42ba1c4b8c1aa83f1fa
William Martin <williammartincruz@gmail.com>
2014-06-28 23:47:25 -0500

    refactored true false values in second argument and converted indentation to spaces

--- logic_operator_lesson.php
+++ logic_operator_lesson.php
@@ -1,19 +1,20 @@
 <?php
 
-$won_lottery= FALSE;
+$won_lottery= 'FALSE';
 $is_raining = 'TRUE';
 
 if ($won_lottery && $is_raining) {
-	 echo "\$I'm buying a new car! \n";
+     echo "I'm buying a new car! \n";
 }
 
-$is_logged_in = false;
-$is_editor = true;
+$is_logged_in = 'FALSE';
+$is_editor = 'TRUE';
 
- if ($is_logged_in && $is_admin) {
-	echo " You can administer users\n";
+if ($is_logged_in && $is_admin) {
+    echo "You can administer users\n";
 }
 
 if ($is_editor || $is_editor) {
-	echo "You can edit this post";
+    echo "You can edit this post\n";
 }
+?>
