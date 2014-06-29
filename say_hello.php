b4bdbc389a238aa063a5f76c39fd3d6ae2eb44fb
William Martin <williammartincruz@gmail.com>
2014-06-29 16:05:03 -0500

    removed comment, uncommented line 6 and commented line 5

--- say_hello.php
+++ say_hello.php
@@ -1,14 +1,13 @@
 <?php
 
-// say hello to the user
-
 if ($argc < 2) {
-	$progName = $argv[0];
-	echo "Usage: $progName [your name] \n";
-	// echo "Hey! I need your name to say hello to you! \n";
-	exit(1);
-	# code...
+    $progName = $argv[0];
+    // echo "Usage: $progName [your name] \n";
+    echo "Hey! I need your name to say hello to you! \n";
+    exit(1);
+   
 }
 $name = $argv[1];
 
 echo "Hello " . $name . "\n";
+?>
