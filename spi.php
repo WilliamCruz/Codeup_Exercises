732b7843fbadffc39763e24c58967090ece55e95
William Martin <williammartincruz@gmail.com>
2014-06-29 16:42:31 -0500

    removed unnecessary comments

--- spi.php
+++ spi.php
@@ -1,26 +1,23 @@
 <?php
 
-// // Write the output
-// // Notice the space after the ?
+//Write the output
 fwrite(STDOUT, 'What is your first name? ');
 
-// // Get the input from user
+//Get the input from user
 $first_name = fgets(STDIN);
 
-// // Output the user's name
+//Output the user's name
 fwrite(STDOUT, "Hello $first_name\n");
 
 // Write the output
-// Notice the space after the ?
 fwrite(STDOUT, 'Guess a number? ');
 
-// Get the input from user
 $number = (int)fgets(STDIN);
 
 if ($number % 2 == 0) {
-	fwrite(STDOUT, "{$number} is even.\n");
+    fwrite(STDOUT, "{$number} is even.\n");
 } else {
-	fwrite(STDOUT, "{$number} is odd.\n");
+    fwrite(STDOUT, "{$number} is odd.\n");
 }
 
 ?>
